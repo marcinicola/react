ed0e283d45b606b52ad56b47e8310337e2e6e942
Nicola Marci <nicola.marci1996@gmail.com>
2023-10-04 19:59:44 +0200

    Esercizio 17

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import './App.css'
 import { Welcome } from './Welcome'
 
 function App() {
- 
 
   return (
     <>
-      <Welcome name= "Nicola"/> 
+      <Welcome/> 
+      {/* Se togliamo la props name non viene renderizzato il nome ma solamente Welcome! */}
+      {/* Se aggiungo name= "Nicola" a <Welcome/> sovrascrive la props della funzione e mi renderizza in pagina Welcome, Nicola altrimenti, Welcome,Nome Mancante */}
     </>
   )
 }
