5d12f6d3f0b73c74965ceccd079ea26bd0dbc0ec
Nicola Marci <nicola.marci1996@gmail.com>
2023-10-04 19:48:37 +0200

    Esercizio 16

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import { Welcome } from './Welcome'
 
 function App() {
  
 
   return (
     <>
-     
+      <Welcome name= "Nicola"/> 
     </>
   )
 }
