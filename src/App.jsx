7e03cd52700b40a640b4cbee5e07b87f5cc09d75
Nicola Marci <nicola.marci1996@gmail.com>
2023-10-04 19:27:14 +0200

    Esercizio 14

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
-import { useState } from 'react'
-import './App.css'
-import { Hello } from './Hello'
-import { Message } from './Message'
+import { useState } from "react";
+import "./App.css";
+import { Hello } from "./Hello";
 
 function App() {
- 
+  function Message() {
+    return <p>What a beautiful day!</p>;
+  }
 
   return (
     <>
-     <Hello/>
-     <Message/>
+      <Hello />
+      {Message()}
+      {/* Si renderizza in pagina lo stesso */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
