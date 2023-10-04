1700d0c7fb248f376cf3b056361bd0b97a7b73fc
Nicola Marci <nicola.marci1996@gmail.com>
2023-10-04 19:20:17 +0200

    Esercizio 13

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 import './App.css'
+import { Hello } from './Hello'
 
 function App() {
  
 
   return (
     <>
-     
+     <Hello/>
+     <Hello/>
+      {/* Renderizza Hello World due volte */}
     </>
   )
 }
