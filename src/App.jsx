32f4ed0e55ce6494d0172dce2c0296140166515e
Nicola Marci <nicola.marci1996@gmail.com>
2023-10-04 20:09:41 +0200

    Esercizio 18

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,7 @@ function App() {
 
   return (
     <>
-      <Welcome/> 
-      {/* Se togliamo la props name non viene renderizzato il nome ma solamente Welcome! */}
-      {/* Se aggiungo name= "Nicola" a <Welcome/> sovrascrive la props della funzione e mi renderizza in pagina Welcome, Nicola altrimenti, Welcome,Nome Mancante */}
+      <Welcome name="Nicola" age={27}/> 
     </>
   )
 }
