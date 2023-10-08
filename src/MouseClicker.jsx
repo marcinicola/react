56fbee6d7b1842d7c031079701f4cf836be97b15
Nicola Marci <nicola.marci1996@gmail.com>
2023-10-08 14:45:26 +0200

    Esercizio 37

--- src/MouseClicker.jsx
+++ src/MouseClicker.jsx
@@ -1,8 +1,12 @@
-export function MouseClicker(){
-    function handleEvent(event){
-            console.log(event.target.name);
-    }
-    return(
-        <button name="oneButton" onClick={handleEvent}>Click Me!</button>
-    )
+export function MouseClicker() {
+  function handleEvent(event) {
+    console.log(event.currentTarget.name);
+
+  }
+  return (
+    <button name="oneButton" onClick={handleEvent}>
+      <img width={24} height={24} />
+      Click Me!
+    </button>
+  );
 }
