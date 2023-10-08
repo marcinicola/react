2de123ac35d3efefabde96cd0e48f0aaff588a39
Nicola Marci <nicola.marci1996@gmail.com>
2023-10-08 16:24:11 +0200

    Esercizio 39

--- src/MouseClicker.jsx
+++ src/MouseClicker.jsx
@@ -1,21 +1,13 @@
 export function MouseClicker() {
   function handleEvent(event) {
-    console.log(event.target);
-
+    console.log(event.target.name);
   }
   function handleEventImg(event) {
-    console.log(event.target.scr);
-
+    console.log(event.currentTarget.src);
   }
   return (
-    
-    /* Perche' Quando fai clic sull'immagine, 
-    l'evento si scatena anche al pulsante genitore, 
-    quindi entrambi gli eventi vengono eseguiti. */
-
-    <button name="oneButton" onClick={handleEvent}>
+    <button name="one" onClick={handleEvent}>
       <img src="#" onClick={handleEventImg} width={24} height={24} />
-      Click Me!
     </button>
   );
 }
