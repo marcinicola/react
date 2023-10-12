8bcc5ee51fbb15ee5d7d3a5b2496c6b18bc3ccf7
Nicola Marci <nicola.marci1996@gmail.com>
2023-10-12 16:43:09 +0200

    Esercizio 42

---
+++ src/Login.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+
+export function Login() {
+  const [data, setData] = useState({
+    username: "",
+    password: "",
+    remember: false,
+  });
+
+  const handleEventInput = (e) => {
+    const name = e.target.name;
+    const value =
+      e.target.type === "checkbox" ? e.target.checked : e.target.value;
+
+    setData((data) => {
+      return {
+        ...data,
+        [name]: value,
+      };
+    });
+  };
+
+  return (
+    <>
+      <form>
+        <input
+          type="text"
+          name="username"
+          value={data.username}
+          onChange={handleEventInput}
+        />
+        <input
+          type="password"
+          name="password"
+          value={data.password}
+          onChange={handleEventInput}
+        />
+        <input
+          type="checkbox"
+          name="remember"
+          checked={data.remember}
+          onChange={handleEventInput}
+        />
+      </form>
+    </>
+  );
+}
