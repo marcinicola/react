6dd7d20fc1d6b89fd4dad10b745614af69430f3a
Nicola Marci <nicola.marci1996@gmail.com>
2023-10-18 14:49:26 +0200

    Esercizio 69

--- src/Container.jsx
+++ src/Container.jsx
@@ -1,8 +1,23 @@
+import { useState } from "react";
+
 export function Container({ children, title }) {
+  const [collapsed, setCollapsed] = useState(true);
+
+  const handleToggle = () => {
+    setCollapsed((t) => !t);
+  };
+
   return (
     <div className="container">
-      <div className="container-title">{title}</div>
-      <div className="container-content">{children}</div>
+      <div>
+        <button onClick={handleToggle}>Toggle</button>
+      </div>
+      {collapsed && (
+        <>
+          <div className="container-title">{title}</div>
+          <div className="container-content">{children}</div>
+        </>
+      )}
     </div>
   );
 }
