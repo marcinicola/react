dee16a1d473a706bad824c506e31d9b296991c7e
Nicola Marci <nicola.marci1996@gmail.com>
2023-10-18 11:37:58 +0200

    Esercizio 67

--- src/Welcome.jsx
+++ src/Welcome.jsx
@@ -1,8 +1,11 @@
+import { Container } from "./Container.jsx";
+import "./Container.css";
+
 export function Welcome({ name, age }) {
   return (
-    <div>
+    <Container>
       <p>Welcome,{name}</p>
       <p>Your age is {age}</p>
-    </div>
+    </Container>
   );
 }
