1434a55480d3d96ab7532a01280d15955ce03c2f
Nicola Marci <nicola.marci1996@gmail.com>
2023-10-18 12:33:23 +0200

    Esercizio 26

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import { AlertClock } from './AlertClock.jsx'
+import App from "./App"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AlertClock />
+    <App />
   </React.StrictMode>,
 )
