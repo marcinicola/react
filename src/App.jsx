61aac98e06d5803444db557a272a23fbbc4e2da0
Nicola Marci <nicola.marci1996@gmail.com>
2023-10-19 16:05:08 +0200

    Esercizio 70

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
-import './App.css'
-import { Clock } from './Clock'
+import { Clock } from "./Clock";
+import { LanguageContext } from "./LanguageContext";
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-
+  const [language, setLanguage] = useState("it");
+  const handleSetLangueage = (e) => {
+    setLanguage(e.target.value);
+  };
   return (
-    <>
-    <Clock />
-    </>
-  )
+    <div>
+      <select value={language} onChange={handleSetLangueage}>
+        <option value="it">Italiano</option>
+        <option value="en">English</option>
+      </select>
+      <LanguageContext.Provider value={language}>
+        <Clock />
+      </LanguageContext.Provider>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/Clock.jsx
+++ src/Clock.jsx
@@ -1,7 +1,9 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
+import { LanguageContext } from "./LanguageContext";
 
 export function Clock() {
   const [currentTime, SetCurrentTime] = useState("");
+  const language = useContext(LanguageContext);
   const data = () => {
     const currentTime = new Date().toLocaleTimeString();
     return currentTime;
@@ -10,18 +12,17 @@ export function Clock() {
   useEffect(() => {
     const interval = setInterval(() => {
       SetCurrentTime(data());
-    }, 1000)
+    }, 1000);
 
-        return() => {
-            clearInterval(interval)
-        }
-
+    return () => {
+      clearInterval(interval);
+    };
   }, []);
 
   return (
     <>
-      <h2>Current Clock</h2>
-        <p>{currentTime}</p>
+      {language === "it" ? <h2>Ora Corrente</h2> : <h2>Current Clock</h2>}
+      <p>{currentTime}</p>
     </>
   );
 }
