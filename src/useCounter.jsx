64b4e61919ea0ea3be2a4b9569d45e907c34852d
Nicola Marci <nicola.marci1996@gmail.com>
2023-10-22 16:43:07 +0200

    Esercizio 75

--- src/useCounter.jsx
+++ src/useCounter.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useState } from "react";
+import { useState } from "react";
 
 export const useCounter = (initialValue = 0) => {
   const [counter, setCounter] = useState(initialValue);
